0f11df6faa552bb535e1bc093fa774d1beba457a
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-12 15:02:36 -0500

    Slight edit to describe kind of tree.

--- HtDP_2e_Exercise_5.rkt
+++ HtDP_2e_Exercise_5.rkt
@@ -4,7 +4,7 @@
 ;Exercise 5. Use the 2htdp/image library to create the image of a simple boat or tree. Make sure you can easily change the scale of the entire image. 
 
 
-;tree
+;Christmas tree
 (require 2htdp/image)
 
 (define TREE-SIZE 400)
