d924de888f9183ee7fbf5716703c1e15932653ab
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-12 15:32:15 -0500

    1.4 The Arithmetic of Images

--- HtDP_2e_Exercise_5.rkt
+++ HtDP_2e_Exercise_5.rkt
@@ -4,7 +4,7 @@
 ;Exercise 5. Use the 2htdp/image library to create the image of a simple boat or tree. Make sure you can easily change the scale of the entire image. 
 
 
-;Christmas tree
+;a Christmas tree
 (require 2htdp/image)
 
 (define TREE-SIZE 400)
@@ -13,16 +13,16 @@
                (/ TREE-SIZE 5) (/ TREE-SIZE 10)
                (rectangle (/ TREE-SIZE 10)(/ TREE-SIZE 2) "solid" "brown"))
 
-;boat
+;a boat
 
 (define BOAT-SIZE 100)
 
 
-;mast and sail design adjusted from tree above
+;the mast and sail are adapted from tree above
 (above (overlay/xy    (triangle (* BOAT-SIZE 1.5) "solid" "red")
                       (* BOAT-SIZE .75) (/ BOAT-SIZE 10)
                       (rectangle (/ BOAT-SIZE 25) (* BOAT-SIZE 1.25) "solid" "brown"))
-;hull
+;the hull
        (beside (triangle/sas (/ BOAT-SIZE 4) 90 (/ BOAT-SIZE 4) "solid" "blue")
                (rectangle (* BOAT-SIZE 1.25) (/ BOAT-SIZE 4) "solid" "blue")
                (triangle/ass 90 (/ BOAT-SIZE 4) (/ BOAT-SIZE 4) "solid" "blue")))
