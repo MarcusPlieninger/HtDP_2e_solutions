2086d38c304106d12fddd72bee6fa3b251456da6
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-13 12:33:16 -0500

    1.7 Predicates: Know Thy Data

---
+++ HtDP_2e_Exercise_10.rkt
@@ -0,0 +1,4 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_10) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 10. Now relax, eat, sleep, and then tackle the next chapter
