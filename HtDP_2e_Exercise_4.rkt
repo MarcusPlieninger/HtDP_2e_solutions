239d5d09055089c10e72142b87090fc534ca9553
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-13 11:11:34 -0500

    1.3 Mixing It Up with Strings

--- HtDP_2e_Exercise_4.rkt
+++ HtDP_2e_Exercise_4.rkt
@@ -1,8 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 ;Exercise 4. Use the same setup as in exercise 3 to create an expression that deletes the ith position from str.
-;Clearly this expression creates a shorter string than the given one. Which values for i are legitimate?
+;Clearly this expression creates a shorter string than the given one.
+;Which values for i are legitimate?
 
 (define str "helloworld")
 (define ind "0123456789")
