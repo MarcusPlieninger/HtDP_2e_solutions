e020362f6fe0987fedf93e66ab774d197beffa03
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-14 13:09:46 -0500

    2.1 Functions

---
+++ HtDP_2e_Exercise_15.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_15) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 15. Define ==>. The function consumes two Boolean values, call them sunny and friday.
+;Its answer is #true if sunny is false or friday is true.
+;Note Logicians call this Boolean operation implication, and they use the notation sunny => friday for this purpose.
+
+(define (=> sunny friday)
+  (or (not sunny) friday))
+
+;Note: Look up definition of "implication" and any associated truth table.
