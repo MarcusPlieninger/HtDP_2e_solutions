c237ec5f2b24e6019e3b507b8b1be0a6ddb35ec9
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-14 13:02:11 -0500

    2.1 Functions

---
+++ HtDP_2e_Exercise_13.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_13) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 13. Define the function string-first, which extracts the first 1String from a non-empty string.
+
+(define (string-first x)
+  (if (>= (string-length x) 1)
+      (substring x 0 1)
+      "The string is empty."))
