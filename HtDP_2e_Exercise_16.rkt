087024f4864cdc03d51f47829698dbebe3876cc8
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-14 13:13:18 -0500

    2.1 Functions

---
+++ HtDP_2e_Exercise_16.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_16) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 16. Define the function image-area, which counts the number of pixels in a given image.
+;See exercise 6 for ideas.
+
+(define (image-area img)
+  (* (image-width img) (image-height img)))
