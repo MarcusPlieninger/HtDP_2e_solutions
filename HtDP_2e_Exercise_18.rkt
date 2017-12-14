5bf7405b4bb0b7821c8ea936dcdb09b47feaaab4
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-14 16:27:44 -0500

    2.1 Functions

---
+++ HtDP_2e_Exercise_18.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_18) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 18. Define the function string-join,
+;which consumes two strings and appends them with "_" in between.
+;See exercise 2 for ideas.
+
+(define (string-join x y)
+  (string-append x "_" y))
+  
