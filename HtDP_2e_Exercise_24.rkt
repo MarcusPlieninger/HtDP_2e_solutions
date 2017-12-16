4e8c656275eed520a2144cdd08cbf02a674c038a
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-15 22:43:26 -0500

    2.2 Computing--not complete

---
+++ HtDP_2e_Exercise_24.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_24) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 24. Here is the definition of ==>:
+
+(define (==> x y)
+  (or (not x) y))
+
+;Use the stepper to determine the value of (==> #true #false). 
+
+(==> #true #false)
