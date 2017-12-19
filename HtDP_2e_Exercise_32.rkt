b0a3fd1b09f49b34ab78e41d2b210643198fae7d
MarcusPlieninger <mwp2101@columbia.edu>
2017-12-18 21:16:50 -0500

    2.5 Programs [review]

---
+++ HtDP_2e_Exercise_32.rkt
@@ -0,0 +1,36 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_32) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 32. Most people no longer use desktop computers just to run applications
+;but also employ cell phones, tablets, and their cars’ information control screen.
+;Soon people will use wearable computers in the form of intelligent glasses, clothes,
+;and sports gear. In the somewhat more distant future, people may come with built-in
+;bio computers that directly interact with body functions.
+;
+;Think of ten different forms of events that software applications
+;on such computers will have to deal with.
+
+;Ten different forms of events that applications software applications will have to
+;deal with on built-in bio computers:
+
+;1. High blood pressure
+
+;2. Onset of seizure
+
+;3. Sleep patters (e.g., apnea, disturbances, etc.)
+
+;4. Blood composition (e.g., elevated glucose levels in blood)
+
+;5. Heart rate (e.g., arrhythmia)
+
+;6. Breathing (e.g., irregular)
+
+;7. Hearing loss
+
+;8. Vision loss
+
+;9. Activation of brain regions
+
+;10.Damage to nervous system
+
+;11. Urinary output and excretion
