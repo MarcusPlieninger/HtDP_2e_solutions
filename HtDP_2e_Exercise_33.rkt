ed30638b2297285ea8b62fdadc3437adae0de8fb
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-19 10:24:35 -0500

    3. How to Design Programs [come back to]

---
+++ HtDP_2e_Exercise_33.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_33) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;Exercise 33. Research the “year 2000” problem and what it meant for software developers. 
+
