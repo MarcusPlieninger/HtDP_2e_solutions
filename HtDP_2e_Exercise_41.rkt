b2a5dd6a50fc03005bd546a780e4ec7870cb7c63
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-22 15:18:56 -0500

    3.6 Designing World Programs

--- HtDP_2e_Exercise_41.rkt
+++ HtDP_2e_Exercise_41.rkt
@@ -24,6 +24,8 @@
 (define WHEEL-RADIUS 10)                    ;this is the single point of control
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 2))
 (define Y-CAR (* WHEEL-RADIUS 3))
+(define ROAD-LENGTH (* 7 (* WHEEL-RADIUS 8)))
+(define SCENE-HEIGHT (* WHEEL-RADIUS 5))
 
 
 ; GRAPHICAL CONSTANTS
@@ -44,17 +46,24 @@
 ; CAR is a computed constant comprised of WHEEL and CHASSIS
 (define CAR
   (overlay/align/offset "middle" "bottom"
-   BOTH-WHEELS
-   0 (* WHEEL-RADIUS -1)
-   CHASSIS))
+                        BOTH-WHEELS
+                        0 (* WHEEL-RADIUS -1)
+                        CHASSIS))
 
 (define BACKGROUND
-  (empty-scene (* 7 (* WHEEL-RADIUS 8)) (* WHEEL-RADIUS 5)))
+  (empty-scene ROAD-LENGTH SCENE-HEIGHT))
 
-; (define TREE
-;   (underlay/xy (circle 10 "solid" "green")
-;                9 15
-;                (rectangle 2 20 "solid" "brown")))
+(define TREE
+  (underlay/xy (circle 10 "solid" "green")
+               9 15
+               (rectangle 2 20 "solid" "brown")))
+
+(define BACKGROUND-TREE
+  (place-image TREE (* .75 ROAD-LENGTH) 32.5 BACKGROUND))
+; -32.5 to account for total height of TREE at 35 pixels
+; and the fact that place-image calculates from center of image.
+; Thus, you substract 17.5 from SCENE-HEIGHT since y axis increases
+; positively as you go downward.
 
 ; A WorldState is a Number.
 ;; interpretation: the number of pixels between the left border of the scene
@@ -67,10 +76,10 @@
 ; WorldState --> Image
 ; places the image of the car in the BACKGROUND 
 ; according to the given world state
-(check-expect (render 50) (place-image CAR 50 Y-CAR BACKGROUND))
-(check-expect (render 200) (place-image CAR 200 Y-CAR BACKGROUND))
+(check-expect (render 50) (place-image CAR 50 Y-CAR BACKGROUND-TREE))
+(check-expect (render 200) (place-image CAR 200 Y-CAR BACKGROUND-TREE))
 (define (render ws)
-  (place-image CAR ws Y-CAR BACKGROUND))
+  (place-image CAR ws Y-CAR BACKGROUND-TREE))
 
 ;    BACKGROUND)
 
@@ -82,18 +91,30 @@
 (define (tock ws)
   (+ ws 3))
 
+;;;end?
+; WorldState --> Boolean
+; ends the world when CAR has travelled ROAD-LENGTH
+(check-expect (last-world? 0) false);testing the extreme
+(check-expect (last-world? 300) false);testing a middle point
+(check-expect (last-world? (+ ROAD-LENGTH (* 4 WHEEL-DISTANCE))) true);testing for desired outcome
+(define (last-world? ws)
+ (> (tock ws) (+ ROAD-LENGTH (* 4 WHEEL-DISTANCE))))
+; I needed to account for the fact that the place-image works from
+; the center of the image. Therefore, I needed to take into account the
+; fact that the car still needs to travel one half car length before it
+; disappears completely off the right side of the scene.
+
+
 ;;;main
 ; WorldState --> WorldState
 ; launches the world with car on left side of background
 (define (main ws)
-   (big-bang ws
-     [on-tick tock]
-     [to-draw render]))
+  (big-bang ws
+    [on-tick tock]
+    [to-draw render]
+    [stop-when last-world?]))
 
-;;;end?
-; WorldState --> Boolean
-; ends the world when the car reaches right side of background
-  
+
 
 
 
