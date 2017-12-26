c482891471e0778e666e22cfcb060658e9e5c403
Marcus Plieninger <mwp2101@columbia.edu>
2017-12-26 01:37:02 -0500

    3.6 Designing World Programs [part 1 complete only]

--- HtDP_2e_Exercise_43.rkt
+++ HtDP_2e_Exercise_43.rkt
@@ -24,44 +24,108 @@
 ; constants duplicated from Exercise 41
 
 
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; "PHYSICAL" CONSTANTS
+
+(define WHEEL-RADIUS 10)                    ;this is the single point of control
+(define WHEEL-DISTANCE (* WHEEL-RADIUS 2))
+(define Y-CAR (* WHEEL-RADIUS 3))
+(define ROAD-LENGTH (* 7 (* WHEEL-RADIUS 8)))
+(define SCENE-HEIGHT (* WHEEL-RADIUS 5))
+(define SPEED 3)                            ;this sets the speed in pixels per clock tick
+
+
+; GRAPHICAL CONSTANTS
+
+
+(define WHEEL                                                         
+  (circle WHEEL-RADIUS "solid" "black"))    
+(define SPACE                             
+  (rectangle WHEEL-DISTANCE (/ WHEEL-RADIUS 8) "solid" "white")) ;divided by 8 to make bottom more flush
+;Note: I need to read up on a potentially better way of doing this since I don't know why I needed to do this.
+(define BOTH-WHEELS                       
+  (beside WHEEL SPACE WHEEL))
+
+(define CHASSIS
+  (above
+   (rectangle (* WHEEL-DISTANCE 2) WHEEL-RADIUS "solid" "red")
+   (rectangle (* WHEEL-DISTANCE 4) (* WHEEL-RADIUS 2) "solid" "red")))
+
+; CAR is a computed constant comprised of WHEEL and CHASSIS
+(define CAR
+  (overlay/align/offset "middle" "bottom"
+                        BOTH-WHEELS
+                        0 (* WHEEL-RADIUS -1)
+                        CHASSIS))
+
+(define BACKGROUND
+  (empty-scene ROAD-LENGTH SCENE-HEIGHT))
+
+(define TREE
+  (underlay/xy (circle WHEEL-RADIUS "solid" "green")
+               (- WHEEL-RADIUS 1) (* WHEEL-RADIUS 1.5)
+               (rectangle (/ WHEEL-RADIUS 5) (* WHEEL-RADIUS 2) "solid" "brown")))
+
+(define BACKGROUND-TREE
+  (place-image TREE (* .75 ROAD-LENGTH) (- SCENE-HEIGHT (/ (image-height TREE) 2)) BACKGROUND))
+
 ; An AnimationState is a Number.
 ; interpretation the number of clock ticks since the animation started
 
 ; WISH LIST
 
-; render
-; AnimtationState --> Image
+;;;;render
+; AnimationState --> Image
 ; places the image of the car in the background according to the accumulating number of clock ticks
 ; (define (render as) <image>);
-; (check-expect (render 0) (place-image/align 
-; (check-expect (render 100) (place-image/align
+(check-expect (render 0) (place-image/align CAR (distance 0) Y-CAR "right" "center" BACKGROUND-TREE))
+(check-expect (render 100) (place-image/align CAR (distance 100) Y-CAR "right" "center" BACKGROUND-TREE))
+(define (render as)
+  (place-image/align CAR (distance as) Y-CAR "right" "center" BACKGROUND-TREE))
 
+;;;distance function definition for use by render
+; Number --> Number
+; takes the number of clock ticks and multiplies it by number of pixels travelled per clock tick
+; distance = time * speed
+; (define (distance as) 10)
+(check-expect (distance 4) 12)
+(check-expect (distance 100) 300)
+(define (distance as)
+  (* SPEED as))
 
-; tock
+;;;tock
 ; AnimationState --> Number
-; the number of clock ticks since the animation started is returned as a number
+; advance the number of clock ticks by one
 ; (define (tock as) 4)
-(check-expect (tock 0) 0)
-(ceck-expect (tock 100) 100)
+(check-expect (tock 0) 1)
+(check-expect (tock 100) 101)
 (define (tock as)
-  as)
+  (+ as 1))
 
-; end?
+;;;end?
 ; AnimationState --> Boolean
-; ends the animation when the car is disappears off the screen
+; ends the world when CAR has travelled ROAD-LENGTH, disappearing off the right side of the screen
+(check-expect (last-world? 0) false);testing the extreme
+(check-expect (last-world? (/ (/ ROAD-LENGTH 2) SPEED)) false);testing a middle point in terms of physical constant
+(check-expect (last-world? (+ ROAD-LENGTH (* 4 WHEEL-DISTANCE))) true);testing for desired outcome in terms of physical constants
+(define (last-world? as)
+ (> (distance as) (+ ROAD-LENGTH (* 4 WHEEL-DISTANCE))))
 
-; big-bang
-; AnimationState --> AnimationState
-; launches the world with car just outside of the left border of the scene
+  
+;;;big-bang
+; AnimationState --> WorldState
+; launches the world with car on left side of background
+(define (main ws)
+  (big-bang ws
+    [on-tick tock]
+    [to-draw render]
+    [stop-when last-world?]))
 
 
-(require 2htdp/image)
-(require 2htdp/universe)
-
-; design of functions tock and render
 
 
-; develop a big-bang expression
 
 
 ; This program relates to animate from Prologue: How to Program? in the following way:
