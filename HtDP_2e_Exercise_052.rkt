5453d69a4064bd83b78d1e7cc13ed5a10f76a2af
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-12 17:30:20 -0500

    4.4 Intervals

---
+++ HtDP_2e_Exercise_052.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_052) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 52. What integers are contained in the four intervals below?
+
+; 1. [3,5] is a closed interval. -> contains 4
+
+
+; 2. (3,5] is a left-open interval. -> contains 3 and 4
+
+
+; 3. [3,5) is a right-open interval. -> contains 4 and 5
+
+
+; 4. (3,5) is an open interval. -> conatins 3, 4 and 5
