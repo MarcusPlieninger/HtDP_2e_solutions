bcbce5d3e7f5cc9f5c5bf29fb56d673e5c2a04ea
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-12 17:35:19 -0500

    4.4 Intervals [counterintuitive]

--- HtDP_2e_Exercise_052.rkt
+++ HtDP_2e_Exercise_052.rkt
@@ -3,13 +3,16 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_052) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; Exercise 52. What integers are contained in the four intervals below?
 
-; 1. [3,5] is a closed interval. -> contains 4
+; 1. [3,5] is a closed interval. -> contains 3, 4 and 5
 
 
-; 2. (3,5] is a left-open interval. -> contains 3 and 4
+; 2. (3,5] is a left-open interval. -> contains 4 and 5
 
 
-; 3. [3,5) is a right-open interval. -> contains 4 and 5
+; 3. [3,5) is a right-open interval. -> contains 3 and 4
 
 
-; 4. (3,5) is an open interval. -> conatins 3, 4 and 5
+; 4. (3,5) is an open interval. -> conatins 4 only
+
+; Observation: "Closed" and "open" are counter-intuitive definition.
+; "Closed" *includes* the boundary. "Open" *excludes* the boundary.
