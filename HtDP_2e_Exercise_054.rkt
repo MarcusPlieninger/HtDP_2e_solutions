0828a5acb69e1aac83a39981684b38b899cb5f05
Marcus Plieninger <mwp2101@columbia.edu>
2018-01-17 16:53:44 -0500

    4.5 Itemizations [review]

--- HtDP_2e_Exercise_054.rkt
+++ HtDP_2e_Exercise_054.rkt
@@ -1,15 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_054) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; Exercise 54. Why is (string=? "resting" x) incorrect as the first condition in show?
-; Conversely, formulate a completely accurate condition, that is, a Boolean expression
-; that evaluates to #true precisely when x belongs to the first sub-class of LRCD.
-
 ; (define (show x)
 ;  (cond
 ;    [(string? x) ...]
 ;    [(<= -3 x -1) ...]
 ;    [(>= x 0) ...]))
+
+; Exercise 54. Why is (string=? "resting" x) incorrect as the first condition in show?
+; Conversely, formulate a completely accurate condition, that is, a Boolean expression
+; that evaluates to #true precisely when x belongs to the first sub-class of LRCD.
+
+; It is incorrect as the first condition of show because show does not accept String arguments.
 
 (define (show x)
   (cond
