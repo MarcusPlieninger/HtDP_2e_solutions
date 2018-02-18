1b25b01ee950034c43cc9a907c4eabae2b5e4dbc
Marcus Plieninger <mwp2101@columbia.edu>
2018-02-18 16:26:53 -0500

    4.2 Computing Conditionally

--- HtDP_2e_Exercise_048.rkt
+++ HtDP_2e_Exercise_048.rkt
@@ -1,10 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_48) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDP_2e_Exercise_048) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; Exercise 48. Enter the definition of reward followed by (reward 18)
 ; into the definitions area of DrRacket and use the stepper to find out
 ; how DrRacket evaluates applications of the function.
-
 
 (define (reward s)
   (cond
